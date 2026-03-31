47c9861e7a54fe226f02b6db327fcbab3752d3c8
ketlynkauany <ketlynlove13@gmail.com>
2026-03-31 16:17:26 -0300

    003

---
+++ cores.ts
@@ -0,0 +1,37 @@
+const cores = ['Azul','Verde', 'Roxo', 'Preto']
+
+
+//1
+
+//console.log("Todas as cores que temos:")
+
+//for (let i = 0; i < cores.length; i++) {
+    //console.log(cores[i]);
+//}
+
+
+//2
+
+
+//cores.splice(2, 1);
+    //console.log(cores);
+
+
+
+//3
+
+
+let corBuscada = "Roxo";
+let encontrado = false;
+
+for (let i = 0; i < cores.length; i++) {
+    if (cores[i] === corBuscada) {
+        console.log("Encontrado na posição:", i);
+        encontrado = true;
+        break;
+    }
+}
+
+if (!encontrado) {
+    console.log("Cor não encontrada");
+}
